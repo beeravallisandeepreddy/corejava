effc17e924419fda4055cebf882934522ac83c26
beeravallisandeepreddy <beeravallisandeepreddy@gmail.com>
2022-02-16 08:13:13 +0530

    Adding some more java files

--- Test1.java
+++ Test1.java
@@ -2,6 +2,8 @@ class Test1
 {
 public static void main (String args[])
 {
+System.out.println("main method start");
+	
 Test1 obj = new Test1();
 obj.simpleInterest(10000,16,3);
 System.out.println("main method");
@@ -13,5 +15,7 @@ public void simpleInterest(int p, int t, int r)
 int x= (p*t*r)/100;
 System.out.println ("Simple Interest " +x);
 System.out.println("method");
+
+System.out.println("method end");
 }
 }
